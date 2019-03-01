4b0c4aa5a58fd95a9d4374fd7a6dca62dcbb4581
Zhang <504947985@qq.com>
2019-03-01 14:19:24 +0800

    sspanel_v3

---
+++ app/Models/Node.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * Node Model
+ */
+
+use App\Utils\Tools;
+
+class Node extends Model
+
+{
+    protected $table = "ss_node";
+
+
+    public function getLastNodeInfoLog()
+    {
+        $id = $this->attributes['id'];
+        $log = NodeInfoLog::where('node_id', $id)->orderBy('id', 'desc')->first();
+        if ($log == null) {
+            return null;
+        }
+        return $log;
+    }
+
+    public function getNodeUptime()
+    {
+        $log = $this->getLastNodeInfoLog();
+        if ($log == null) {
+            return "暂无数据";
+        }
+        return Tools::secondsToTime((int)$log->uptime);
+    }
+
+    public function getNodeLoad()
+    {
+        $log = $this->getLastNodeInfoLog();
+        if ($log == null) {
+            return "暂无数据";
+        }
+        return $log->load;
+    }
+
+    public function getLastNodeOnlineLog()
+    {
+        $id = $this->attributes['id'];
+        $log = NodeOnlineLog::where('node_id', $id)->orderBy('id', 'desc')->first();
+        if ($log == null) {
+            return null;
+        }
+        return $log;
+    }
+
+    function getOnlineUserCount()
+    {
+        $log = $this->getLastNodeOnlineLog();
+        if ($log == null) {
+            return "暂无数据";
+        }
+        return $log->online_user;
+    }
+
+    function getTrafficFromLogs()
+    {
+        $id = $this->attributes['id'];
+        $traffic = TrafficLog::where('node_id', $id)->sum('u') + TrafficLog::where('node_id', $id)->sum('d');
+        if ($traffic == 0) {
+            return "暂无数据";
+        }
+        return Tools::flowAutoShow($traffic);
+    }
+
+    public static function getCustomerMethod()
+    {
+        return [
+            "rc4-md5" => "rc4-md5",
+            "aes-128-cfb" => "aes-128-cfb",
+            "aes-192-cfb" => "aes-192-cfb",
+            "aes-256-cfb" => "aes-256-cfb",
+            "des-cfb" => "des-cfb",
+            "bf-cfb" => "bf-cfb",
+            "cast5-cfb" => "cast5-cfb",
+            "chacha20" => "chacha20",
+            "salsa20" => "salsa20",
+        ];
+    }
+}
